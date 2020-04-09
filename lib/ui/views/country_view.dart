1b5467c7366e7c5bae5915c53e06c64ff8bbb0ba
Arnab Banerjee <38814484+hurbes@users.noreply.github.com>
2020-04-09 12:43:17 +0530

    tried to fix refresh issue on country page

--- lib/ui/views/country_view.dart
+++ lib/ui/views/country_view.dart
@@ -24,83 +24,84 @@ class _CountryViewState extends State<CountryView>
         viewModel: CountryViewModel(),
         onModelReady: (model) => model.getCountryCases(),
         builder: (context, model, child) {
-          return Container(
-            color: !widget.call
-                ? Theme.of(context).backgroundColor
-                : Colors.transparent,
-            child: Padding(
-              padding: !widget.call
-                  ? const EdgeInsets.fromLTRB(15, 15, 15, 5)
-                  : const EdgeInsets.all(0),
-              child: Column(
-                children: <Widget>[
-                  !widget.call
-                      ? Container(
-                          height: 60,
-                          decoration: BoxDecoration(
-                              color: Theme.of(context).cardColor,
-                              borderRadius: BorderRadius.circular(10)),
-                          child: Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Center(
-                              child: TextField(
-                                cursorColor: Colors.grey.withOpacity(0.4),
-                                style: TextStyle(
-                                    color: Theme.of(context)
-                                        .primaryColor
-                                        .withOpacity(0.6),
-                                    fontWeight: FontWeight.w300,
-                                    fontSize: 20),
-                                decoration: InputDecoration(
-                                    border: InputBorder.none,
-                                    hintText: "Search",
-                                    hintStyle: TextStyle(
-                                        color: Theme.of(context)
-                                            .primaryColor
-                                            .withOpacity(0.6),
-                                        fontWeight: FontWeight.w300,
-                                        fontSize: 20)),
-                                onChanged: (val) =>
-                                    model.filterSearchResults(val.trim()),
+          return RefreshIndicator(
+            onRefresh: () => model.getCountryCases(),
+                      child: Container(
+              color: !widget.call
+                  ? Theme.of(context).backgroundColor
+                  : Colors.transparent,
+              child: Padding(
+                padding: !widget.call
+                    ? const EdgeInsets.fromLTRB(15, 15, 15, 5)
+                    : const EdgeInsets.all(0),
+                child: Column(
+                  children: <Widget>[
+                    !widget.call
+                        ? Container(
+                            height: 60,
+                            decoration: BoxDecoration(
+                                color: Theme.of(context).cardColor,
+                                borderRadius: BorderRadius.circular(10)),
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Center(
+                                child: TextField(
+                                  cursorColor: Colors.grey.withOpacity(0.4),
+                                  style: TextStyle(
+                                      color: Theme.of(context)
+                                          .primaryColor
+                                          .withOpacity(0.6),
+                                      fontWeight: FontWeight.w300,
+                                      fontSize: 20),
+                                  decoration: InputDecoration(
+                                      border: InputBorder.none,
+                                      hintText: "Search",
+                                      hintStyle: TextStyle(
+                                          color: Theme.of(context)
+                                              .primaryColor
+                                              .withOpacity(0.6),
+                                          fontWeight: FontWeight.w300,
+                                          fontSize: 20)),
+                                  onChanged: (val) =>
+                                      model.filterSearchResults(val.trim()),
+                                ),
                               ),
                             ),
-                          ),
-                        )
-                      : SizedBox(),
-                  verticalSpaceMedium,
-                  !model.busy
-                      ? Expanded(
-                          child: model.country.isNotEmpty
-                              ? RefreshIndicator(
-                                  onRefresh: () => model.getCountryCases(),
-                                  child: ListView.builder(
-                                    physics:
-                                        const AlwaysScrollableScrollPhysics(),
-                                    shrinkWrap: true,
-                                    itemCount: widget.limit != null
-                                        ? widget.limit
-                                        : model.country.length,
-                                    itemBuilder: (context, i) {
-                                      return Padding(
-                                        padding: const EdgeInsets.only(
-                                            top: 0.0, bottom: 15),
-                                        child: SummaryWidgetItem(
-                                            active: model.country[i].active,
-                                            cases: model.country[i].cases,
-                                            critical: model.country[i].critical,
-                                            death: model.country[i].deaths,
-                                            name: model.country[i].country,
-                                            recovered:
-                                                model.country[i].recovered,
-                                            flag: model
-                                                .country[i].countryInfo.flag),
-                                      );
-                                    },
-                                  ),
-                                )
-                              : ErrorText())
-                      : Loading()
-                ],
+                          )
+                        : SizedBox(),
+                    verticalSpaceMedium,
+                    !model.busy
+                        ? Expanded(
+                            child: model.country.isNotEmpty
+                                ? ListView.builder(
+                                      physics:
+                                          const AlwaysScrollableScrollPhysics(),
+                                      shrinkWrap: true,
+                                      itemCount: widget.limit != null
+                                          ? widget.limit
+                                          : model.country.length,
+                                      itemBuilder: (context, i) {
+                                        return Padding(
+                                          padding: const EdgeInsets.only(
+                                              top: 0.0, bottom: 15),
+                                          child: SummaryWidgetItem(
+                                              active: model.country[i].active,
+                                              cases: model.country[i].cases,
+                                              critical: model.country[i].critical,
+                                              death: model.country[i].deaths,
+                                              name: model.country[i].country,
+                                              recovered:
+                                                  model.country[i].recovered,
+                                              flag: model
+                                                  .country[i].countryInfo.flag),
+                                        );
+                                      },
+                                    )
+                                  
+                                : ErrorText())
+                        : Loading()
+                  ],
+                ),
               ),
             ),
           );
